53c130727c0a67b664501ffabf23d401c340915e
r1ch <rich@bradi.sh>
2020-04-21 18:47:26 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ Vue.component('google-login', {
 
 Vue.component('ysgtb-container',{
 	mixins:[APIMixin],
-	inject['profile'],
+	inject:['profile'],
 	data: function(){
 		return {
 			attendee : {
