0c64250eb0e9b4f188e42b0123b91dbf82b5d15d
r1ch <rich@bradi.sh>
2020-04-22 12:18:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,16 +45,8 @@ Vue.component('ysgtb-container',{
 	template:`
 		<div class="jumbotron" v-if = "profile.ready">
 			<div class="container">
-				<form>
-					<div class="form-row">
-						<div class="col">
-							<input @change = "newAttendee" class="form-control form-control-lg col-12" type="text" v-model="attendee.name">
-						</div>
-						<div class="col">
-							<span class = "display-4">still {{go}} to Blue Coat</span>
-						</div>
-					</div>
-				</form>
+				<input @change = "newAttendee" class="form-control form-control-lg col-6 attendee-name" type="text" v-model="attendee.name">
+				<span class = "display-4">still {{go}} to Blue Coat</span>
 				<br><br>
 				<p class="lead" v-if = "attendee.reporter">Thanks for letting us know {{attendee.reporter}}</p>
 				<small v-if = "time">and it's been over {{time.duration}} {{time.measure}} now</small>
