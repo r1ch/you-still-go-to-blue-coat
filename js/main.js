0861c896db43cfa4d91ce6795ae93dc584be53a8
r1ch <rich@bradi.sh>
2020-04-26 20:33:22 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -115,11 +115,11 @@ Vue.component('ysgtb-jumbotron',{
 			let h = Math.floor(seconds % (3600*24) / 3600);
 			let m = Math.floor(seconds % 3600 / 60);
 			let s = Math.floor(seconds % 60);
-			let dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
-			let hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-			let mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+			let dDisplay = d > 0 ? d + (d == 1 ? " day" : " days") : "";
+			let hDisplay = h > 0 ? h + (h == 1 ? " hour" : " hours") : "";
+			let mDisplay = m > 0 ? m + (m == 1 ? " minute" : " minutes") : "";
 			let sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-			return dDisplay + hDisplay + mDisplay + sDisplay;
+			return [dDisplay,hDisplay,mDisplay,sDisplay].filter(item=>item!="").join(", ")
 		}
 	},
 	mounted: function(){
