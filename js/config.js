25a8752d31e146df7695785a205d53efecacc19a
r1ch <rich@bradi.sh>
2020-04-26 15:09:16 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.config = {
     roleArn: "arn:aws:iam::312837731527:role/BlueCoatGoogleOAuthRole",
     region: "eu-west-1",
-    apiGatewayUrl : "https://oa32bun9tk.execute-api.eu-west-1.amazonaws.com",
+    apiGatewayUrl : "https://xyrm1h5e1h.execute-api.eu-west-1.amazonaws.com",
     apiGatewayPath : "/prod",
 }
