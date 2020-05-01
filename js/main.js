1cfc19601ce3fcf1c23410396a9744f9bfc1d27d
r1ch <rich@bradi.sh>
2020-05-01 15:41:47 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -200,7 +200,7 @@ Vue.component('ysgtb-time', {
 			}
 		}
 	},
-	template:`<span v-if = "millis" v-html="short?time.text:time.html"></span>`
+	template:`<span v-if = "millis" v-html="short?time.html:time.text"></span>`
 })
 
 var app = new Vue({
