ba8a570defee47cf49550cf213b415c65626c0d2
r1ch <rich@bradi.sh>
2020-05-04 11:19:57 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ Vue.component('ysgtb-jumbotron',{
 		<div>
 			<div class="jumbotron" v-if = "attendee">
 				<div class="container">
-					<input @keyup = "newAttendee" class="form-control form-control-lg col-6 col-md-3 attendee-name" type="text" v-model="attendee.name" :disabled = "!profile.ready">
+					<input @keyup = "newAttendee" class="form-control form-control-lg col-6 col-md-3 attendee-name" type="text" v-model="attendee.name" :disabled = "!profile.ready" id="theName">
 					<span class = "display-4">&nbsp;still {{go}} to Blue Coat</span>
 					<br><br>
 					<p class="lead" v-if = "attendee.reporter">Thanks for letting us know {{attendee.reporter}}</p>
