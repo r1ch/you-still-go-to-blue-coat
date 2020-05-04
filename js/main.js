b4a3f4ad510a6f3d837ebca8f1fa1796bcf6556d
r1ch <rich@bradi.sh>
2020-05-04 20:35:55 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -211,7 +211,7 @@ Vue.component('ysgtb-d3', {
 			if (this.times.length == 0) return;
 			let t = d3.transition().duration(750);
 			
-			let colourScale = d3.scaleOrdinal().domain([... new Set(this.times.map(time=>time.name))]).range(d3.schemeSet2);
+			let colourScale = d3.scaleOrdinal().domain([... new Set(this.times.map(time=>time.name))]).range(d3.schemeSet1);
 			
 			let xScale = d3.scaleTime()
 				.domain([this.times[0].from,this.times[this.times.length-1].to])
