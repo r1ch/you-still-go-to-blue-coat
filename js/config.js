6855ad0e42a45739d63f6d7b50372b99b1f2792d
r1ch <rich@bradi.sh>
2020-05-04 11:13:21 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
 window.config = {
+    googleClientId: "",
     roleArn: "arn:aws:iam::312837731527:role/BlueCoatGoogleOAuthRole",
     region: "eu-west-1",
     apiGatewayUrl : "https://xyrm1h5e1h.execute-api.eu-west-1.amazonaws.com",
