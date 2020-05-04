bb7c3953c17d185e6ed4577a5b3bc2cda000f299
r1ch <rich@bradi.sh>
2020-05-04 21:23:13 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -216,8 +216,8 @@ Vue.component('ysgtb-d3', {
 				.range([0, this.width])
 
 			let xAxis = d3.axisBottom(xScale)
-				.ticks(20)
-				.tickFormat(20)
+				.ticks(xScale.ticks(20))
+				.tickFormat(xScale.ticksFormat())
 
 			this.svg.select(".x")
 				.transition(t)
