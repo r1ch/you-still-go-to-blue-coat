01740eebbdb756f2635098650cfe607aa198a20e
r1ch <rich@bradi.sh>
2020-05-09 19:53:34 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,9 +49,10 @@ Vue.component('ysgtb-jumbotron',{
 			<div class = "container" v-if = "attendances.length > 0">
 				<h4>Grew in grace</h4>
 				<ul class="list-group">
-					<li class="list-group-item flex-column align-items-start" v-for = "attendance in orderedAttendances" :class= "{active:attendee.name == attendance.identifier, 'image-background':attendee.name == attendance.identifier}">
+					<li class="list-group-item flex-column align-items-start" v-for = "(attendance, index) in orderedAttendances" :class= "{active:attendee.name == attendance.identifier, 'image-background':attendee.name == attendance.identifier}">
 						<div class="d-flex w-100 justify-content-between">
 							<h5 class="mb-1"><span :style="{color:colourScale(attendance.identifier[0])}">•</span>&nbsp;{{attendance.identifier}}</h5>
+							<<span class="badge badge-pill badge-dark d-none d-sm-block">{{["Mr Inches' favourite","",""][index]}}</span>
 							<ysgtb-time :short = "true" :millis = "attendance.record"></ysgtb-time>
 						</div>
 						<div class="d-flex w-100 justify-content-between">
