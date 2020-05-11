6f3a331f6e3d7aef82e9936fd3a6442769ccfd8d
r1ch <rich@bradi.sh>
2020-05-11 17:27:30 +0100

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -207,7 +207,7 @@ Vue.component('ysgtb-d3', {
 				.call(yAxis)
 			
 			let lineGenerator = name => {
-				let attendance = attendances.find(attendance=>attendance.identifier==name)
+				let attendance = this.attendances.find(attendance=>attendance.identifier==name)
 				let unexplained = timeBlocks[timeBlocks.length-1].totals[name] - (attendance.record || 0)
 				d3.line()
     				.x(d=>d.end)
