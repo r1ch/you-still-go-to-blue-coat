6120038f7f8d54a7d86240c69de86f4885709914
r1ch <rich@bradi.sh>
2020-05-11 17:38:36 +0100

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -212,7 +212,7 @@ Vue.component('ysgtb-d3', {
 				return d3.line()
     				.x(d=>d.end)
     				.y(d=>{
-					console.log(`${d} ${unexplained}`
+					console.log(`${d} ${unexplained}`)
 					return yScale((d.totals[name] || 0) + unexplained)
 				})
    				.curve(d3.curveMonotoneX)
