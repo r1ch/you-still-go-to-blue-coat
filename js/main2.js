d905bad81600b7df0512460d6c668d5870efabfb
r1ch <rich@bradi.sh>
2020-05-12 10:26:26 +0100

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -221,7 +221,7 @@ Vue.component('ysgtb-d3', {
 			let lineGenerator = name => {
 				return d3.line()
     				.x(d=>d.end)
-    				.y(d=>yScale(d.totals[name])
+    				.y(d=>yScale(d.totals[name]))
    				.curve(d3.curveMonotoneX)
 			}
 
