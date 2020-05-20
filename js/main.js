93f0b57072977ff4d5de394b334530a09b09f3c3
r1ch <rich@bradi.sh>
2020-05-20 14:52:09 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -359,11 +359,12 @@ var app = new Vue({
 	computed: {
 		orderedAttendances: function(){
 			let currentAttendance = this.attendances.find(attendance=>attendance.identifier==this.attendee.name)
+			console.log(JSON.stringify(currentAttendance))
 			return this.attendances
 			.map(attendance=>{
 				let a = {...attendance}
-				a.record += (this.attendee.name == a.identifier ? this.now-this.attendee.identifier : 0)
-				if(currentAttendance && this.attendee.name != a.identifier) a.lead = a.record - currentAttendance.record
+				a.record += (this.attendee.name == attendance.identifier ? this.now-this.attendee.identifier : 0)
+				if(currentAttendance && this.attendee.name != attendance.identifier) a.lead = attendance.record - currentAttendance.record
 				return a
 			})
 			.sort((a,b)=>b.record-a.record)
