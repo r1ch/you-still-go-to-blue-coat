6736fb55aaf54fea6ca6c5b90ad24d0fd79ea0c7
r1ch <rich@bradi.sh>
2020-05-20 15:00:38 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -358,7 +358,7 @@ var app = new Vue({
 	},
 	computed: {
 		orderedAttendances: function(){
-			let currentAttendance = this.attendances.find(attendance=>attendance.identifier==this.attendee.name)
+			let currentAttendance = this.attendances.find(attendance=>attendance.identifier==this.attendee.name) || {}
 			currentAttendance.record += this.now-this.attendee.identifier
 			return this.attendances
 			.map(attendance=>{
