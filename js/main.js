5f6b7ee64b47bbccb55705c2ace39688987a6a62
r1ch <rich@bradi.sh>
2020-05-21 11:08:24 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -187,6 +187,7 @@ Vue.component('ysgtb-d3', {
 			let xScale = d3.scaleTime()
 				.domain([this.times[0].from,this.times[this.times.length-1].to])
 				.range([0, this.width])
+				.nice()
 
 			let xAxis = d3.axisBottom(xScale)
 				.ticks(this.ticks)
