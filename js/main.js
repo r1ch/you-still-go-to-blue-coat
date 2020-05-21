9b8b40cc5da8505ef5e1ca1cf970d3f62e97cf7c
r1ch <rich@bradi.sh>
2020-05-21 11:12:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -185,7 +185,7 @@ Vue.component('ysgtb-d3', {
 			const t = this.svg.transition().duration(this.drawCount > (8|16|32)  ? 750 : 0)
 			
 			let xScale = d3.scaleTime()
-				.domain([this.times[0].from,this.times[this.times.length-1].to]).nice()
+				.domain([this.times[0].from,this.times[this.times.length-1].to])
 				.range([0, this.width])
 
 			let xAxis = d3.axisBottom(xScale)
