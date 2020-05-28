ec2bc2fd86388351b3e41afc6037c896718575f3
covidnineteen <65950391+covidnineteen@users.noreply.github.com>
2020-05-28 12:30:02 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -369,12 +369,7 @@ var app = new Vue({
 				return a
 			})
 			.sort((a,b)=>b.record-a.record)
-		},
-		adjustedTimes : function(){
-			let times = this.times.slice(0)
-			times[times.length-1].to = this.now
-			return times
-		}
+		}	
 	},
 	methods:{
 		update:  _.throttle(function(){
@@ -458,7 +453,7 @@ var app = new Vue({
 				:colourScale="colourScale">
 			</ysgtb-jumbotron>
 			<ysgtb-d3 
-				:times = "adjustedTimes"
+				:times = "times"
 				:attendances = "orderedAttendances"
 				:profile="profile"
 				:colourScale="colourScale"
