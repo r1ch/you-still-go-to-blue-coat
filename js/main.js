662d67c0d5f8614a5e44f2d6a3fd1dbd31476f5a
r1ch <rich@bradi.sh>
2020-10-02 13:56:49 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -327,11 +327,9 @@ Vue.component('ysgtb-d3', {
 				.data(this.visits)
 				.join(enter=>enter.append('circle'))
 				.attr('class', d=>`visitLabel ${d.identifier}`)
-				.attr('r', 2.5)
-				.attr('fill',d=>d.identifier.indexOf("Guest") == -1 ? this.colourScale(d.identifier[0]) : "none")
-                                .attr('stroke',d=>d.identifier.indexOf("Guest") > -1 ? this.colourScale(d.identifier[0]) : "none")
-				.attr('stroke-width',d=>d.identifier.indexOf("Guest") > -1 ? "1px" : 0)
-				.attr('cy', this.barHeight+5)
+				.attr('r',d=>2 - d.identifier.indexOf("Guest")/2)
+				.attr('fill',d=>this.colourScale(d.identifier[0]))
+				.attr('cy', d=>5 + this.barHeight - d.identifier.indexOf("Guest")/2)
 				.attr('cx', d=>xScale(d.time))
 
 			return true;
