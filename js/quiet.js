17a4f2a9dcf0a528f2bab77dfab8cf1567e230d5
r1ch <rich@bradi.sh>
2020-10-07 12:52:59 +0100

    Update quiet.js

--- js/quiet.js
+++ js/quiet.js
@@ -2,7 +2,7 @@ window.config.QUIET = true
 
 const checkVersion = ()=>{
   fetch("https://ysgtb.bradi.sh/version")
-  .then(console.log)
+  .then(response=>console.log(JSON.stringify(response)))
 }
 
 setInterval(checkVersion,500)
