c6e1161dbbcfae763a1aeefa1bc52ed634ae0430
r1ch <rich@bradi.sh>
2020-10-07 12:59:21 +0100

    Update quiet.js

--- js/quiet.js
+++ js/quiet.js
@@ -4,7 +4,7 @@ const checkVersion = ()=>{
   fetch(`https://ysgtb.bradi.sh/version?${Date.now()}`)
   .then(response=>{
     console.log(response.status)
-    console.log(response.text())
+    response.text().then(console.log)
   })
   .catch(err=>console.error(err))
 }
