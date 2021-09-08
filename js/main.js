400c58014b7c1e6337a071db313c02df9401446e
r1ch <rich@bradi.sh>
2021-09-08 14:34:42 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,7 +27,28 @@ Vue.component('ysgtb-jumbotron',{
 		<div>
 			<div class="jumbotron" v-if = "attendee">
 				<div class="container">
-					<input @keyup = "newAttendee" class="form-control form-control-lg col-6 col-md-3 attendee-name" type="text" v-model="attendee.name" @click = "startAuthentication" :class = "{'btn-outline-success':!profile.ready}">
+					<input 
+						@keyup = "newAttendee"
+						class="form-control form-control-lg col-6 col-md-3 attendee-name"
+						type="text" v-model="attendee.name"
+						@click = "startAuthentication"
+						:class = "{'btn-outline-success':!profile.ready}"
+					>
+					<div id="g_id_onload"
+					     data-client_id="953648658436-1bql4k0mhfaiqe8p141c4rs0sgsjbcut.apps.googleusercontent.com"
+					     data-context="signin"
+					     data-ux_mode="popup"
+					     data-callback="handleAuthentication"
+					     data-auto_select="true"
+					     data-close_on_tap_outside="false">
+					</div>
+					<div class="g_id_signin"
+					     data-type="icon"
+					     data-shape="circle"
+					     data-theme="outline"
+					     data-text="signin_with"
+					     data-size="small">
+					</div>
 					<span class = "display-4">&nbsp;still {{go}} to Blue Coat</span>
 					<br><br>
 					<p class="lead" v-if = "attendee.reporter"><i>according to {{attendee.reporter}}, <ysgtb-time :mode="'text'" :millis="now-attendee.identifier"></ysgtb-time></i></p>
